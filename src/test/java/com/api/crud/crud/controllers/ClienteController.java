4dac4dcfe5fcbe462093c37122074c64b4ff3d9a
Dcdoca <antoniomarcorrs@gmail.com>
2024-07-24 21:16:11 -0300

    crud

---
+++ src/test/java/com/api/crud/crud/controllers/ClienteController.java
@@ -0,0 +1,43 @@
+package com.api.crud.crud.controllers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import org.springframework.web.bind.annotation.*;
+
+//import com.api.crud.crud.Cliente;
+
+//@RestController
+//@RequestMapping("clientes")
+//public class ClienteController {
+
+    // Get all clients
+    //@GetMapping
+    //public String getAllClientes() {
+        //if(!Cliente.clientes.isEmpty()) {
+            // Usar o Gson para retornar os objetos
+          //  return Cliente.clientes.toString();
+       // } else {
+          //  return "Não há clientes cadastrados.";
+        //}
+    //}
+
+   // @PostMapping
+   // public String addCliente(@RequestBody Cliente cliente) {
+
+      //  System.out.println("Adicionando cliente...");
+
+       // Cliente.clientes.add(cliente);
+       // return "Cliente adicionado com sucesso!";
+   // }
+//}
---
+++ src/test/java/com/api/crud/crud/controllers/exceptions/SenaiApplication.java
@@ -0,0 +1,13 @@
+package com.api.crud.crud.controllers.exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiApplication.class, args);
+
+	}
+    }//
