8d48d32054eefd0081447fa2be7fbdcbc669cdbf
Dcdoca <antoniomarcorrs@gmail.com>
2024-07-24 21:54:05 -0300

    crud resolvido

--- src/test/java/com/api/crud/crud/Cliente.java
+++ src/test/java/com/api/crud/crud/Cliente.java
@@ -6,13 +6,24 @@ import lombok.Data;
 
 @Data
 public class Cliente {
-    public static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-
+    private Long id;
     private String nome;
     private String cpf;
-    private Endereco endereco;
+    private String endereco; // Supondo que endereco seja uma string por simplicidade
     private String telefone;
     private String email;
     private String dataNascimento;
 
+    // Construtores, Getters e Setters
 }
+//public class Cliente {
+   // public static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+   // private String nome;
+    //private String cpf;
+   // private Endereco endereco;
+   // private String telefone;
+   // private String email;
+   // private String dataNascimento;
+
+//}
--- src/test/java/com/api/crud/crud/controllers/ClienteController.java
+++ src/test/java/com/api/crud/crud/controllers/ClienteController.java
@@ -1,4 +1,55 @@
 package com.api.crud.crud.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.api.crud.crud.Cliente;
+import com.api.crud.crud.ClienteService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public List<Cliente> getAllClientes() {
+        return clienteService.getAllClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
+        return clienteService.getClienteById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Cliente createCliente(@RequestBody Cliente cliente) {
+        return clienteService.createCliente(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @RequestBody Cliente clienteDetails) {
+        return clienteService.updateCliente(id, clienteDetails)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
+        if (clienteService.deleteCliente(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
+
+
 
 
 
